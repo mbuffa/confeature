4d672e146fa25c3ab1f81da2676c80da330f306e
Maxime Buffa <mbuffa@gmail.com>
2022-08-17 00:08:34 +0200

    Revert postgres config changes

--- config/test.exs
+++ config/test.exs
@@ -3,8 +3,8 @@ import Config
 config :confeature, Test.Repo,
   hostname: "localhost",
   database: "confeature_test",
-  username: "docker",
-  password: "docker",
+  username: "postgres",
+  password: "postgres",
   port: "5432",
   pool: Ecto.Adapters.SQL.Sandbox,
   show_sensitive_data_on_connection_error: true
