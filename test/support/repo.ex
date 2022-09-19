a324d0e6f4e644e52f86b7439f554399a7dc9a92
Maxime Buffa <mbuffa@gmail.com>
2022-09-20 01:35:01 +0200

    Sanitize configuration

--- test/support/repo.ex
+++ test/support/repo.ex
@@ -4,10 +4,20 @@ defmodule Test.Repo do
     adapter: Ecto.Adapters.Postgres
 
   def init(_type, config) do
-    IO.inspect {:debug, config}
     if System.get_env("CI") == "true" do
       url = "ecto://postgres:postgres@postgres:5432/confeature_test"
-      {:ok, Keyword.put(config, :url, url)}
+      config =
+        config
+        |> Keyword.put(:url, url)
+        |> Keyword.delete(:hostname)
+        |> Keyword.delete(:username)
+        |> Keyword.delete(:password)
+        |> Keyword.delete(:database)
+        |> Keyword.delete(:port)
+        |> Keyword.put(:timeout, 60000)
+
+      IO.inspect {:debug, config}
+      {:ok, config}
     else
       {:ok, config}
     end
