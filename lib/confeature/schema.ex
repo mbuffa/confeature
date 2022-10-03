1b67bed2e4b2b67fca898845e2448b626c91331f
Maxime Buffa <mbuffa@gmail.com>
2022-10-03 15:09:40 +0200

    Fix partial attributes updates (#2)

--- lib/confeature/schema.ex
+++ lib/confeature/schema.ex
@@ -16,8 +16,30 @@ defmodule Confeature.Schema do
   end
 
   def changeset(feature = %__MODULE__{}, params \\ %{}) do
+    new_attrs =
+      params
+      |> Map.fetch!(:attrs)
+      |> Enum.reject(fn {_k, v} -> is_nil(v) end)
+      |> Enum.map(fn
+        {k, v} when is_atom(k) -> {Atom.to_string(k), v}
+        {k, v} -> {k, v}
+      end)
+      |> Enum.into(%{})
+
+    new_attrs =
+      Map.fetch!(feature, :attrs)
+      |> replace_nil_with_map()
+      |> Map.merge(new_attrs)
+
+    params =
+      params
+      |> Map.put(:attrs, new_attrs)
+
     feature
     |> cast(params, [:name, :attrs])
     |> unique_constraint([:name])
   end
+
+  defp replace_nil_with_map(%{} = map), do: map
+  defp replace_nil_with_map(nil), do: %{}
 end
